0a2a042d742cf13b37db1ba0dd17ae93763e3a3e
PaulAnds <paulandressolis@gmail.com>
2023-06-25 14:24:50 -0600

    Add animations and characters

---
+++ Source/SevenDeadlySins/SevenDeadlySinsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SevenDeadlySinsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASevenDeadlySinsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASevenDeadlySinsGameMode();
+};
+
+
+
